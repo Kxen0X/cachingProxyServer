31efe42ae326bd8861f6f0f76bfd66dcf422f82a
Kxen0X <gangstaamirov@gmail.com>
2026-09-16 21:44:55 +0500

    file organization

---
+++ cachingProxy/src/LRUCache.cpp
@@ -0,0 +1,52 @@
+#include "LRUCache.h"
+
+
+LRUCache::LRUCache(size_t mxS ) : currentSize{ 0 }, maxSize{ mxS } {
+
+}
+
+std::optional<std::string> LRUCache::get(const std::string& key) {
+	auto it = iters.find(key);
+	if (it == iters.end()) return std::nullopt;
+	data.splice(data.begin(), data, it->second);
+	return data.front().second;
+}
+
+void LRUCache::add(const std::string& key, std::string Newdata) {
+	if (key.size() + Newdata.size() > maxSize) {
+		std::cout << "THE DATA SIZE IS TOO LARGE TO BE WRITTEN TO THE CACHE";
+		return;
+	}
+	auto it = iters.find(key);
+
+	if (it != iters.end()) {
+		currentSize -= key.size() + (it->second)->second.size();
+		this->data.erase(it->second);
+		iters.erase(key);
+
+	}
+
+	if (currentSize + key.size() + Newdata.size() > maxSize) {
+		while (!this->data.empty() && (currentSize + key.size() + Newdata.size() > maxSize)) {
+			longUsedDataRemove();
+		}
+		std::cout << "INSUFFICIENT CACHE SPACE, DATA THAT HAD NOT BEEN USED FOR A LONG TIME WAS REMOVED" << std::endl;
+	}
+
+	currentSize += key.size() + Newdata.size();
+	this->data.push_front({ key, std::move(Newdata) });
+	iters[key] = this->data.begin();
+
+}
+
+void LRUCache::clear() {
+	data.clear();
+	iters.clear();
+	currentSize = 0;
+}
+
+void LRUCache::longUsedDataRemove() {
+	currentSize -= this->data.back().first.size() + this->data.back().second.size();
+	iters.erase(this->data.back().first);
+	this->data.pop_back();
+}
